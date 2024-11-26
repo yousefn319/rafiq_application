c80be1156f1178c43c7e6317974cdfc3d288a009
omarfaysal1111 <omarfaysal44@gmail.com>
2024-11-26 16:54:39 +0200

    ok

---
+++ lib/features/introscreens/presentation/signup/signup_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:rafiq_application/main.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.sizeOf(context).width;
+    double height = MediaQuery.sizeOf(context).height;
+    return Scaffold();
+  }
+}
